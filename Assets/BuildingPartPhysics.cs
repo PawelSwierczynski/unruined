4a3de19cc6dbeaeeb7ba59518792c4ab0576abb8
Paweł Świerczyński <pawelswierczynski@pro.wp.pl>
2020-03-31 15:14:06 +0200

    Add basic movement

---
+++ Assets/BuildingPartPhysics.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPartPhysics : MonoBehaviour
+{
+    private bool isLeftControlPressed;
+    private bool isRightControlPressed;
+
+    void Start()
+    {
+        isLeftControlPressed = false;
+        isRightControlPressed = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            isLeftControlPressed = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            isRightControlPressed = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isLeftControlPressed)
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            isLeftControlPressed = false;
+        }
+        if (isRightControlPressed)
+        {
+            transform.position += new Vector3(1, 0, 0);
+            isRightControlPressed = false;
+        }
+    }
+}
