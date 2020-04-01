eac2097263b5026a498ccb3f156d7e2e1ab3e67b
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-01 14:54:46 +0200

    Menu scene

---
+++ Assets/Resources/Buttons/StartButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartButton : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
+{
+    public Sprite newSprite;
+    public Sprite oldSprite;
+
+    void Start()
+    {
+
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Mouse entered");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Exit");
+    }
+}
