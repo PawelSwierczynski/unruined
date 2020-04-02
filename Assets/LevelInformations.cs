1c35dfeac4aa9614fd98667bd7367e743cfa55d9
Paweł Świerczyński <pawelswierczynski@pro.wp.pl>
2020-04-02 13:51:25 +0200

    Add some level informations

--- Assets/LevelInformations.cs
+++ Assets/LevelInformations.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LevelLoader;
 
 public class LevelInformations : MonoBehaviour
 {
+    public int LevelIdentifier { get; private set; }
+    public string LevelName { get; private set; }
     public int LeftBorderPositionX { get; private set; }
     public int RightBorderPositionY { get; private set; }
     public int DownBorderPositionY { get; private set; }
+    public List<Coordinates> patternCells { get; private set; }
+    public List<Coordinates> rainCells { get; private set; }
 
     void Start()
     {
         LeftBorderPositionX = 0;
         RightBorderPositionY = 22;
         DownBorderPositionY = 40;
+
+        LevelsDictionary levelsDictionary = new LevelsDictionary(File.ReadAllText("Assets/Resources/Levels/Levels.json"));
+
     }
 }
