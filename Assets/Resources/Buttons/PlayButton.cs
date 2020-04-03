ba009b86055ba7ff4fef4263391fcd9fbb5f18e7
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-03 14:09:27 +0200

    Added scroll and tried level choosing

--- Assets/Resources/Buttons/PlayButton.cs
+++ Assets/Resources/Buttons/PlayButton.cs
@@ -10,7 +10,7 @@ public class PlayButton : MonoBehaviour, IPointerClickHandler
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        if(GameManager.Instance.SelectedLevel == 1)
+        if(GameManager.Instance.SelectedLevel != 0)
         {
             SceneManager.LoadScene("SampleScene");
         }
