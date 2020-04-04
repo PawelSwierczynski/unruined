86d834469362c75737b27d6f647d3bfc0882aadb
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-04 19:21:00 +0200

    added restart button

---
+++ Assets/Resources/Buttons/RestartButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
