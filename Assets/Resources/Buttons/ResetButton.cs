69a3681a0ff600f52b38bf2cf6bbf238c1a97d0e
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-06 14:21:54 +0200

    added reset save button

---
+++ Assets/Resources/Buttons/ResetButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ResetButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameManager.Instance.SaveGameSystem.ResetProgress();
+        GameManager.Instance.SaveGameSystem.SetSoundVolume(SoundManager.Instance.SoundVolume);
+        GameManager.Instance.SaveGame();
+    }
+}
+
+
