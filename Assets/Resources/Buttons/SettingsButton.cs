e68b4d8a7d554ec5ed7db32a3aaf30771b1161a1
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-06 12:14:40 +0200

    volume changing and settings

---
+++ Assets/Resources/Buttons/SettingsButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SettingsButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+}
