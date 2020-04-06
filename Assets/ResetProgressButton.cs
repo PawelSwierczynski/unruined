98cd4fb410fe717fc83a92e3af365b55219060a0
Paweł Świerczyński <pawelswierczynski@pro.wp.pl>
2020-04-06 14:32:20 +0200

    Add reset progress button

--- Assets/ResetProgressButton.cs
+++ Assets/ResetProgressButton.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class AcceptButton : MonoBehaviour, IPointerClickHandler
+public class ResetProgressButton : MonoBehaviour, IPointerClickHandler
 {
     public void OnPointerClick(PointerEventData eventData)
     {
+        GameManager.Instance.SaveGameSystem.ResetProgress();
         GameManager.Instance.SaveGameSystem.SetSoundVolume(SoundManager.Instance.SoundVolume);
         GameManager.Instance.SaveGame();
     }
