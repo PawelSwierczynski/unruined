c7e0c6f793d7bed99fc9808c2442c1b2ec6eabc9
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-06 18:25:32 +0200

    removed newtonsoft

---
+++ Assets/LevelElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LevelLoader
+{
+    [Serializable]
+    public sealed class LevelElement
+    {
+        public int LevelID;
+        public string Name;
+        public List<Coordinates> ExistingBlocks;
+        public List<Coordinates> EmptyBlocks;
+
+        public LevelElement(int levelID, string name, List<Coordinates> existingBlocks, List<Coordinates> emptyBlocks)
+        {
+            LevelID = levelID;
+            Name = name;
+            ExistingBlocks = existingBlocks;
+            EmptyBlocks = emptyBlocks;
+        }
+    }
+}
---
+++ Assets/LevelsDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace LevelLoader
+{
+    [Serializable]
+    public sealed class LevelsDictionary
+    {
+        public List<LevelElement> Levels;
+
+        public LevelsDictionary(string json)
+        {
+            Levels = JsonUtility.FromJson<List<LevelElement>>(json);
+        }
+
+        public LevelsDictionary()
+        {
+            var tupleList = new List<Coordinates>
+                {
+                    new Coordinates(1,2),
+                    new Coordinates(4,5)
+                };
+            Levels = new List<LevelElement> { new LevelElement(1, "First Level", tupleList, tupleList) };
+        }
+    }
+}
