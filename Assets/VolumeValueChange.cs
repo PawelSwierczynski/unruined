3a0dcde40296f3bcf58537f5fcd374473f962c23
SzczepaniakJ <szczepan.szczepan2@gmail.com>
2020-04-06 12:49:10 +0200

    tried fixing slider

---
+++ Assets/VolumeValueChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour
+{
+    private float musicVolume = 1f;
+    void Update()
+    {
+        FindObjectOfType<SoundManager>().SetVolume(musicVolume);
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
